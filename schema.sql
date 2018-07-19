26a37b7db8898793e2b24483b3ddfda7ae3d752e
Shaital Desai <shaital.desai@sbcglobal.net>
2018-07-19 09:11:18 -0700

    /watchlist routes and /search routes refactored

--- schema.sql
+++ schema.sql
@@ -1,39 +1,39 @@
-DROP DATABASE IF EXISTS test;
+-- DROP DATABASE IF EXISTS test;
 
-CREATE DATABASE test;
+-- CREATE DATABASE test;
 
-USE test;
+-- USE test;
 
-CREATE TABLE users (
-  id int NOT NULL AUTO_INCREMENT,
-  userName varchar(10) NOT NULL,
-  email varchar(50) NOT NULL,
-  PRIMARY KEY (id)
-);
+-- CREATE TABLE users (
+--   id int NOT NULL AUTO_INCREMENT,
+--   userName varchar(10) NOT NULL,
+--   email varchar(50) NOT NULL,
+--   PRIMARY KEY (id)
+-- );
 
-CREATE TABLE products (
-  id int NOT NULL AUTO_INCREMENT,
-  itemId int NOT NULL,
-  productName varchar(100) NOT NULL,
-  salesPrice int NOT NULL,
-  PRIMARY KEY (id)
-);
+-- CREATE TABLE products (
+--   id int NOT NULL AUTO_INCREMENT,
+--   itemId int NOT NULL,
+--   productName varchar(100) NOT NULL,
+--   salesPrice int NOT NULL,
+--   PRIMARY KEY (id)
+-- );
 
-CREATE TABLE users_products (
-  id int NOT NULL AUTO_INCREMENT,
-  id_users int NOT NULL,
-  id_products int NOT NULL,
-  PRIMARY KEY (id),
-  FOREIGN KEY (id_users) REFERENCES users(id),
-  FOREIGN KEY (id_products) REFERENCES products(id)
-);
+-- CREATE TABLE users_products (
+--   id int NOT NULL AUTO_INCREMENT,
+--   id_users int NOT NULL,
+--   id_products int NOT NULL,
+--   PRIMARY KEY (id),
+--   FOREIGN KEY (id_users) REFERENCES users(id),
+--   FOREIGN KEY (id_products) REFERENCES products(id)
+-- );
 
-/*  Execute this file from the command line by typing:
- *    mysql -u root < server/schema.sql
- *  to create the database and the tables.*/
+ --  Execute this file from the command line by typing:
+ -- *    mysql -u root < server/schema.sql
+ -- *  to create the database and the tables.
 
 
-/*  Testing the database
+  -- Testing the database
 -- INSERT INTO users (`id`, `userName`, `token`, `email`)
 -- VALUES
 --   (1, 'Shaital', 'mango', 'mango@gmail.com'),
@@ -49,6 +49,76 @@ CREATE TABLE users_products (
 --   (3, 3456, 'iPhone', 12),
 --     (4, 4567, 'iMac', 13)
 -- ;
+
+-- INSERT INTO users_products (`id`, `id_users`, `id_products`)
+-- VALUES
+--     (1, 1, 2),
+--     (2, 2, 3),
+--     (3, 3, 4),
+--     (4, 4, 1)
+-- ;
+
+
+DROP DATABASE IF EXISTS test;
+
+CREATE DATABASE test;
+
+USE test;
+
+CREATE TABLE users (
+  id int NOT NULL AUTO_INCREMENT,
+  token int NOT NULL UNIQUE,
+  userName varchar(10) NOT NULL,
+  email varchar(50) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE products (
+  id int NOT NULL AUTO_INCREMENT,
+  itemId int NOT NULL,
+  productName varchar(100) NOT NULL,
+  salesPrice int NOT NULL,
+  threshHoldPrice int NOT NULL,
+  created_at timestamp NOT NULL,
+  user_id int NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES users(id),
+  PRIMARY KEY (id)
+);
+
+
+CREATE TABLE users_products (
+  id_users int NOT NULL,
+  id_products int NOT NULL,
+  PRIMARY KEY (id_users, id_products),
+  FOREIGN KEY (id_users) REFERENCES users(id),
+  FOREIGN KEY (id_products) REFERENCES products(id)
+);
+
+
+/*  Execute this file from the command line by typing:
+ *    mysql -u root < server/schema.sql
+ *  to create the database and the tables.*/
+
+
+/*  Testing the database
+INSERT INTO users (userName, email) VALUES
+  ('Shaital', 'mango@gmail.com'),
+  (2, 'Nasa', 'raspberry', 'raspberry@gmail.com'),
+  (3, 'Khizra', 'watermelon', 'watermelon@gmail.com'),
+    (4, 'Lukas', 'apple', 'apple@gmail.com')
+;
+
+-- INSERT INTO products (itemID, productName, salesPrice, threshHoldPrice) VALUES
+--   (1234, 'iPad', 20, 10),
+--   (2, 2345, 'iWatch', 11),
+--   (3, 3456, 'iPhone', 12),
+--     (4, 4567, 'iMac', 13)
+-- ;
+
+INSERT INTO users (token, userName, email) VALUES (12345, 'Shaital', 'mango@gmail.com');
+INSERT INTO products (itemID, productName, salesPrice, threshHoldPrice) VALUES (1234, 'iPad', 20, 10);
+
+INSERT INTO users_products (id_users, id_products) SELECT users.id, products.id FROM users INNER JOIN products ON users.id = products.id;
 
 -- INSERT INTO users_products (`id`, `id_users`, `id_products`)
 -- VALUES
